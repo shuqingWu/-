0ff55421f5ee30135a80b2137e46bff9562f9c21
shuqingWu <m13058357821@163.com>
2018-03-26 08:45:51 +0800

    发布框架完成

---
+++ 练习发布框架/练习发布框架/框架/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  练习发布框架
+//
+//  Created by 树清吴 on 2018/3/26.
+//  Copyright © 2018年 DD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+- (void)eat;
+
+@end
---
+++ 练习发布框架/练习发布框架/框架/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  练习发布框架
+//
+//  Created by 树清吴 on 2018/3/26.
+//  Copyright © 2018年 DD. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)eat {
+    NSLog(@"%@ 吃了", self.name);
+}
+
+@end
